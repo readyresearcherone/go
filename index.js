1badd52fffb579b321f5016243e5a250f5f92f57
Cristian <81422291+readyresearcherone@users.noreply.github.com>
2022-08-16 18:49:29 +0100

    added the go board and square selector

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
-const renderer = new THREE.WebGLRenderer();
+        import * as THREE from '/three.js-master/build/three.module.js';
+        import {OrbitControls} from '/three.js-master/examples/jsm/controls/OrbitControls.js';
+        const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -14,6 +14,38 @@ camera.position.set(10, 15, -22);
 
 orbit.update();
 
+const goBoard = new THREE.Mesh (
+    new THREE.PlaneGeometry(19,19),
+    new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        side: THREE.DoubleSide,
+        visible: false
+    })
+)
+
+goBoard.rotateX(Math.PI / 2);
+scene.add(goBoard);
+
+const highlight = new THREE.Mesh (
+    new THREE.PlaneGeometry(1,1),
+    new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        side: THREE.DoubleSide,
+        
+    })
+)
+
+highlight.rotateX(Math.PI / 2);
+scene.add(highlight);
+
+
+
+const grid = new THREE.GridHelper(19, 19)
+scene.add(grid);
+
+
+
+
 function animate() {
     renderer.render(scene, camera);
 }
@@ -25,3 +57,5 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
+      
+
